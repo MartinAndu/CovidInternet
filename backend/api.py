e028547d7539eb586d539b8f23b7bcb046a51fd2
Martin Andújar <mandujar@fi.uba.ar>
2020-07-24 04:22:37 -0300

    Add database retrieval function

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,jsonify
+from tinydb import TinyDB, Query
 from main import main
 import json
 import platform
@@ -16,30 +17,31 @@ def get_json_data(parameters):
 
     # for item in items:
     #     result.append(json.dumps(item))
-    print (parameters["normal"])
     return json.dumps(parameters)
 	
 @app.route('/getJson', methods=['POST', 'GET'])
 def hello_world():
-    print(platform.python_version())
     if request.method == 'POST':
         print('post app')
+        print(request.json)
         req = request.json
-        req.pop(0)
+
         values = dict()
-        weights = dict()       
-        for r in req:
-        	weights[r[0]] = float(r[1])
-        	values[r[0]] = float(r[2])        
+        weights = dict() 
+
+
+        for r in req["services"]:
+        	weights[r] = float(req["weights"][r])
+        	values[r] = float(req["values"][r])        
 
 
         parameters = dict()
         parameters['weights'] = weights
         parameters['values'] = values
-        parameters['avg_mbps_monthly'] = req[0][3]
-        parameters['avg_use'] = req[0][4]
-        parameters['max_cap_tb'] = req[0][5]
-        parameters['increase_covid'] = req[0][6]
+        parameters['avg_mbps_monthly'] = req["parameters"]["avg_mbps_monthly"] # req[0][3]
+        parameters['avg_use'] =  req["parameters"]["avg_use"] # req[0][4]
+        parameters['max_cap_tb'] =  req["parameters"]["max_cap_tb"] # req[0][5]
+        parameters['increase_covid'] = req["parameters"]["increase_covid"] # req[0][6]
 
 
         json_result = get_json_data(main(parameters))
@@ -48,3 +50,19 @@ def hello_world():
 
 
         return json_result
+
+@app.route('/saveJson', methods=['POST', 'GET'])
+def save():
+    if request.method == 'POST':
+        db = TinyDB('db.json')
+        doc_id =db.insert(request.json)
+        request.json["doc_id"] = doc_id
+        db.update(request.json, doc_ids=[doc_id])
+        return json.dumps(db.all())
+
+
+@app.route('/getAll', methods=['GET'])
+def getAll():
+    if request.method == 'GET':
+        db = TinyDB('db.json')
+        return json.dumps(db.all())
